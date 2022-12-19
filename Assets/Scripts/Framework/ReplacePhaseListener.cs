113ceb0821e365a9029c8fbd58d8240d6beee8a6
kaclok <572005238@qq.com>
2022-12-19 17:00:43 +0800

    PHASEListener同一时间不能存在两个激活的，所以就需要一个第三方管理器，在加载第二个listener的时候，将前一个销毁或者disable AudioListener可以存在两个，只是有警告 但是PHASEListener则是直接报错

---
+++ Assets/Scripts/Framework/ReplacePhaseListener.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+#if UNITY_IPHONE || UNITY_IOS || UNITY_EDITOR_OSX
+[DisallowMultipleComponent]
+public class ReplacePhaseListener : ReplaceComponent<Apple.PHASE.PHASEListener> {
+}
+#endif
