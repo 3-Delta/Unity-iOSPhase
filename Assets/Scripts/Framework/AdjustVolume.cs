7d54d6665b8027233c65d3c42616dbb986fdb577
kaclok <572005238@qq.com>
2022-12-29 20:16:43 +0800

    构建切换场景的测试
    fg

--- Assets/Scripts/Framework/AdjustVolume.cs
+++ Assets/Scripts/Framework/AdjustVolume.cs
@@ -1,31 +1,48 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 #if UNITY_IPHONE || UNITY_IOS || UNITY_EDITOR_OSX
 using Apple.PHASE;
+using UnityEngine.SceneManagement;
 
 public class AdjustVolume : MonoBehaviour
 {
     public Slider slider;
-    public PHASESource source;
-    public Rotator r;
-    public Button button;
+    public Text num;
+    
+    public Button button1;
+    public Button button2;
 
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
+        
         slider.onValueChanged.AddListener(OnValueChanged);
-        button.onClick.AddListener(OnClicked);
+        button1.onClick.AddListener(Onbutton1Clicked);
+        button2.onClick.AddListener(Onbutton2Clicked);
     }
 
     private void OnValueChanged(float v)
     {
-        Helpers.PHASEAdjustVolume(source.GetSourceId(), v);
+        foreach (var kvp in PHASESource._registeredSources)
+        {
+            Helpers.PHASEAdjustVolume(kvp.Key, v);
+        }
     }
 
-    private bool flag = true;
-    private void OnClicked() {
-        flag = !flag;
-        r.enabled = flag;
+    private void Update()
+    {
+        num.text = $"sources: {PHASESource._registeredSources.Count.ToString()}";
+    }
+
+    private void Onbutton1Clicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    private void Onbutton2Clicked() {
+        SceneManager.LoadScene(2);
     }
 }
 #else
